531a32ea0e065b29e67a471ff782342431b02fa8
Stephan Mantler <step@stephanmantler.com>
2020-03-03 16:33:30 +0100

    updated package description

--- Package.swift
+++ Package.swift
@@ -2,29 +2,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "jobs-redis-driver",
+    name: "queues-redis-driver",
     platforms: [
        .macOS(.v10_14)
     ],
     products: [
         .library(
-            name: "JobsRedisDriver",
-            targets: ["JobsRedisDriver"]
+            name: "QueuesRedisDriver",
+            targets: ["QueuesRedisDriver"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/jobs.git", from: "1.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/queues.git", from: "1.0.0-beta.3"),
         .package(url: "https://github.com/vapor/redis-kit.git", from: "1.0.0-beta.2"),
     ],
     targets: [
         .target(
-            name: "JobsRedisDriver",
-            dependencies: ["Jobs", "RedisKit"]
+            name: "QueuesRedisDriver",
+            dependencies: ["Queues", "RedisKit"]
         ),
         .testTarget(
-            name: "JobsRedisDriverTests",
-            dependencies: ["JobsRedisDriver", "XCTVapor"]
+            name: "QueuesRedisDriverTests",
+            dependencies: ["QueuesRedisDriver", "XCTVapor"]
         ),
     ]
 )
